23a22b21a139d8952ad840ddf55f50821c354700
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-24 18:11:07 -0700

    Added paragraph generator logic based off input

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ function App () {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log('hello world');
+		let amount = parseInt(count);
+		if (amount <= 0) {
+			amount = 1;
+		}
+		if (amount > data.length) {
+			amount = data.length;
+		}
+		setParagraphs(data.slice(0, amount));
 	};
 
 	return (
@@ -26,8 +33,9 @@ function App () {
 				</button>
 			</form>
 			<article className="lorem-text">
-				<p>lorem ipsums 1</p>
-				<p>lorem ipsums 2</p>
+				{paragraphs.map((paragraph, index) => {
+					return <p key={index}>{paragraph}</p>;
+				})}
 			</article>
 		</section>
 	);
