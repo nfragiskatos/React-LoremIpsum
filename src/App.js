8dcb9e8c4275160fcd15c21a879dd79f62a69058
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-24 17:42:29 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import data from './data';
+function App () {
+	const [ count, setCount ] = useState(0);
+	const [ paragraphs, setParagraphs ] = useState([]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('hello world');
+	};
+
+	return (
+		<section className="section-center">
+			<h3>tired of boring lorem ipsum?</h3>
+			<form className="lorem-form" onSubmit={handleSubmit}>
+				<label htmlFor="amount">paragraphs:</label>
+				<input
+					type="number"
+					name="amount"
+					id="amount"
+					value={count}
+					onChange={(e) => setCount(e.target.value)}
+				/>
+				<button type="submit" className="btn">
+					generate
+				</button>
+			</form>
+		</section>
+	);
+}
+
+export default App;
